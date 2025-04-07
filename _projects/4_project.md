bf5c768d20c893ee0a1908371619e43ccc3644d9
Luocheng Huang <luocheng@meta.com>
2025-04-07 14:43:33 -0700

    Fig docker bug, add bibs

---
+++ _projects/4_project.md
@@ -0,0 +1,13 @@
+---
+layout: page
+title: 3D Models
+description: 3D models I designed
+importance: 1
+category: Modeling
+---
+
+[Modular Watch Stand](https://www.printables.com/model/563681-gridfinity-watch-stand)
+
+[Modular MagSafe iPhone Stand](https://www.printables.com/model/567797-gridfinity-magsafe-stand)
+
+[Parameterized Ring/Glasses Stand](https://www.printables.com/model/567497-ring-holderorganizer-gridfinity-1x1)
--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -2,6 +2,11 @@
 
 CONFIG_FILE=_config.yml 
 
+# Install missing gems first
+echo "Installing Jekyll gems..."
+bundle install
+
+# Start Jekyll
 /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
 while true; do
@@ -15,6 +20,8 @@ while true; do
     jekyll_pid=$(pgrep -f jekyll)
     kill -KILL $jekyll_pid
 
+    # Install gems again in case dependencies changed
+    bundle install
     /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
   fi
